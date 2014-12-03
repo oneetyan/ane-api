35ebf575141627890e6df797da05d2dd2a9b67c0
aereal <aereal@aereal.org>
2014-12-03 19:11:29 +0900

    reload

--- lib/ane/web.rb
+++ lib/ane/web.rb
@@ -3,6 +3,11 @@ require 'sinatra/base'
 
 module Ane
   class Web < Sinatra::Base
+    configure :development do
+      require 'sinatra/reloader'
+      register Sinatra::Reloader
+    end
+
     helpers do
       def json(obj)
         content_type 'application/json; charset=utf-8' # XXX
